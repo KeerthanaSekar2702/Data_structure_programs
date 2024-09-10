1c7c0e2a5ac9758af7398fdc70d4c01eb264ffc1
KeerthanaSekar2702 <keerthanasekar418@gmail.com>
2024-09-10 10:22:30 +0530

    binary_tree.c

---
+++ binary_tree.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+Node* insertNode(Node* root, int data) {
+    if (root == NULL) {
+        return createNode(data);
+    }
+    
+    if (data < root->data) {
+        root->left = insertNode(root->left, data);
+    } else if (data > root->data) {
+        root->right = insertNode(root->right, data);
+    }
+    return root;
+}
+void inOrderTraversal(Node* root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d ", root->data);
+        inOrderTraversal(root->right);
+    }
+}
+
+void freeTree(Node* root) {
+    if (root != NULL) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
+}
+
+int main() {
+    Node* root = NULL;
+    int numNodes, data;
+
+    printf("Enter the number of nodes to insert: ");
+    scanf("%d", &numNodes);
+
+    for (int i = 0; i < numNodes; i++) {
+        printf("Enter node %d data: ", i + 1);
+        scanf("%d", &data);
+        root = insertNode(root, data);
+    }
+
+    printf("In-order traversal of the binary tree:\n");
+    inOrderTraversal(root);
+    printf("\n");
+
+    freeTree(root);
+
+    return 0;
+}
